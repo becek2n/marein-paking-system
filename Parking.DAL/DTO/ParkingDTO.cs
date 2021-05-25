e20fa2b6fffe2992589fdba19f93283871ef1fbd
Djanuar <januar.aransyah@gmail.com>
2021-05-26 02:04:52 +0700

    model dto

---
+++ Parking.DAL/DTO/ParkingDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.DTO
+{
+    public class ParkingDTO
+    {
+        public int Id { get; set; }
+        public string TicketCode { get; set; }
+        public int TransportationId { get; set; }
+        public string PlateNumberFirst { get; set; }
+        public string PlateNumberMiddle { get; set; }
+        public string PlateNumberLast { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime? CheckOut { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public double Amount { get; set; }
+        public TransportationDTO Transportation { get; set; }
+    }
+
+    public class TransportationDTO {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
