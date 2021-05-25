31af2c95d7346a744751cd5be0f4e269450d26e8
Djanuar <januar.aransyah@gmail.com>
2021-05-26 02:10:57 +0700

    implement dependency injection using unity.mvc5 package

---
+++ Parking.UI/App_Start/UnityConfig.cs
@@ -0,0 +1,35 @@
+using AutoMapper;
+using Parking.DAL;
+using Parking.Interfaces;
+using System.Web.Mvc;
+using Unity;
+using Unity.Mvc5;
+
+namespace Parking.UI
+{
+    public static class UnityConfig
+    {
+        public static void RegisterComponents()
+        {
+			var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<IParking, ParkingDAL>();
+            //automammper
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            container.RegisterInstance(mapper);
+
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
