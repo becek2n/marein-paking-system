da0e1877adabe5b8bc2ee842f4ee1eb070700bdf
Djanuar <januar.aransyah@gmail.com>
2021-05-26 02:07:47 +0700

    helper write log error

---
+++ Parking.Helper/Logging.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Helper
+{
+    public static class Logging
+    {
+        public static void WriteErr(Exception objError)
+        {
+            StreamWriter oSw;
+            string sErrMsg = "";
+            FileStream fileStream = null;
+            DirectoryInfo logDirInfo = null;
+            FileInfo logFileInfo;
+
+            try
+            {
+                if (!Directory.Exists(SiteConfig.LOG))
+                {
+                    Directory.CreateDirectory(SiteConfig.LOG);
+                }
+
+                string _logFilePath = SiteConfig.LOG + System.DateTime.Today.ToString("yyyy-MM-dd") + "." + "txt";
+                logFileInfo = new FileInfo(_logFilePath);
+                logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                if (!logFileInfo.Exists)
+                {
+                    fileStream = logFileInfo.Create();
+                }
+                else
+                {
+                    fileStream = new FileStream(_logFilePath, FileMode.Append);
+                }
+                oSw = new StreamWriter(fileStream);
+                oSw.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " [E] " + objError.Message);
+                oSw.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " [E] " + objError.InnerException);
+                oSw.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss") + " [E] " + objError.StackTrace);
+                oSw.WriteLine();
+                oSw.WriteLine("==========================================================================================================");
+                oSw.WriteLine();
+                oSw.Close();
+
+            }
+            catch (Exception ex)
+            {
+                sErrMsg = ex.Message;
+            }
+        }
+    }
+}
---
+++ Parking.Helper/SiteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.Helper
+{
+    public static class SiteConfig
+    {
+        public static string LOG
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings["Log"];
+            }
+        }
+    }
+}
