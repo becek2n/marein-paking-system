d587cdf000435379d38022c8bb03be38b9052659
Djanuar <januar.aransyah@gmail.com>
2021-05-26 02:06:48 +0700

    automapper for mapping between object repository and DTO

---
+++ Parking.DAL/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Parking.DTO;
+using Parking.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parking.DAL
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ParkingArea, ParkingDTO>();
+            CreateMap<ParkingDTO, ParkingArea>();
+            CreateMap<Transportation, TransportationDTO>();
+        }
+    }
+}
