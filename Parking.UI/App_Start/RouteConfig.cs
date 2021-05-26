f9653b4ddbe2d0bfdc7f129a8b102485492bc38d
Djanuar <januar.aransyah@gmail.com>
2021-05-27 00:44:16 +0700

    confirm box in action

--- Parking.UI/App_Start/RouteConfig.cs
+++ Parking.UI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Parking.UI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Parking", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
